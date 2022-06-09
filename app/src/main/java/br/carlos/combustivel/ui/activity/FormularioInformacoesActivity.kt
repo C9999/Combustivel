b0a14072e143f7b1cfab2eecd7cc8504fe5deb8b
C9999 <car_soad@hotmail.com>
2022-06-09 17:44:11 -0300

    ajuste de build e layout

---
+++ app/src/main/java/br/carlos/combustivel/ui/activity/FormularioInformacoesActivity.kt
@@ -0,0 +1,13 @@
+package br.carlos.combustivel.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.carlos.combustivel.R
+
+class FormularioInformacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_informacoes)
+    }
+}
